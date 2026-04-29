f2b658685decf644492a0dc864be6e4697a58370
mike7d7 <mike7d7@proton.me>
2026-04-29 16:06:33 -0600

    first commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
+  };
+
+  outputs =
+    inputs@{
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.x86_64-linux.default =
+        let
+          kernel = pkgs.cachyosKernels.linux-cachyos-latest.override {
+            # Customize CachyOS settings
+            cpusched = "eevdf";
+            lto = "full";
+            processorOpt = "x86_64-v3";
+            hzTicks = "1000";
+            bbr3 = true;
+            hardened = false;
+            autofdo = true;
+            autoModules = false;
+            # Additional args are available. See kernel-cachyos/mkCachyKernel.nix
+            structuredExtraConfig = {
+              CONFIG_DEBUG_INFO = "n";
+              CONFIG_DEBUG_KERNEL = "n";
+              CONFIG_DEBUG_ATOMIC_SLEEP = "n";
+              CONFIG_DEBUG_SPINLOCK = "n";
+              CONFIG_DEBUG_MUTEXES = "n";
+              CONFIG_DEBUG_RT_MUTEXES = "n";
+              CONFIG_DEBUG_SLAB = "n";
+            };
+          };
+          # helpers.nix provides a few utilities for building kernel with LTO.
+          # I haven't figured out a clean way to expose it in flakes.
+          helpers = pkgs.callPackage "${inputs.nix-cachyos-kernel.outPath}/helpers.nix" { };
+        in
+        helpers.kernelModuleLLVMOverride (pkgs.linuxKernel.packagesFor kernel);
+
+    };
+}
