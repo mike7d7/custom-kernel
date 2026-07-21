7216f547f43665ca93eccbccfff9eb211afa4ccd
mike7d7 <mike7d7@proton.me>
2026-07-21 17:53:41 -0600

    change nix formatter

--- flake.nix
+++ flake.nix
@@ -6,37 +6,33 @@
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nix-cachyos-kernel,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
+  outputs = {
+    self,
+    nixpkgs,
+    nix-cachyos-kernel,
+    ...
+  }: let
+    system = "x86_64-linux";
 
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          nix-cachyos-kernel.overlays.pinned
-        ];
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        nix-cachyos-kernel.overlays.pinned
+      ];
+    };
+  in {
+    packages.${system}.default = let
+      kernelSet = pkgs.cachyosKernels.linux-cachyos-latest.override {
+        cpusched = "eevdf";
+        lto = "full";
+        processorOpt = "x86_64-v3";
+        hzTicks = "1000";
+        bbr3 = true;
+        hardened = false;
+        autofdo = true;
+        autoModules = false;
       };
     in
-    {
-      packages.${system}.default =
-        let
-          kernelSet = pkgs.cachyosKernels.linux-cachyos-latest.override {
-            cpusched = "eevdf";
-            lto = "full";
-            processorOpt = "x86_64-v3";
-            hzTicks = "1000";
-            bbr3 = true;
-            hardened = false;
-            autofdo = true;
-            autoModules = false;
-          };
-        in
-        (pkgs.linuxKernel.packagesFor kernelSet).kernel;
-    };
+      (pkgs.linuxKernel.packagesFor kernelSet).kernel;
+  };
 }
